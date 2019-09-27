5cf8fb713eeb760d16d46afe0cb9ff37ce827318
Dario Mercado <dariomercado@RICHARDs-MBP.localdomain>
2019-09-27 09:56:21 -0700

    added a few edits to profile form, and adding static.json

--- src/components/ProfileForm.js
+++ src/components/ProfileForm.js
@@ -74,7 +74,7 @@ class ProfileForm extends Component {
             <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
           </div>
           <div className="form-group">
-            <label htmlFor="password">Re-enter Password</label>
+            <label htmlFor="password">New Password</label>
             <input onChange={this.handleChange} name="password" value={this.state.password} type="password" className="form-control" id="password" placeholder="Password" />
           </div>
           <button type="submit" className="btn btn-primary">Save Changes</button>
